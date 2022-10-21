f689312b632ece089d00ea3355e741efc097c11d
GhislainFievet <ghislain.fievet@gmail.com>
2022-10-21 06:56:02 +0200

    add function

--- R/supertry.r
+++ R/supertry.r
@@ -9,9 +9,36 @@ library(DT)
 library(tidyverse)
 
 func_supertry = function(){
-    message("hello")
+    message("hello 2")
 }
 
 runInitJS <- function(){
     runjs('document.getElementById("sel.panel.mode").parentElement.style.display="none"')
+}
+
+ui <- function(seurat.object) {
+    fluidPage(
+        useShinyjs(),
+        shiny::includeScript(file.path("..","js","script.js")),
+        
+        # titlePanel(UITitlePanel()),
+        UITitlePanel(),
+        fluidRow(
+            column(
+                width = 8,
+                uiOutput("sel.panel"),
+            ),
+            column(
+                width = 4,
+                UIVisPanel(),
+                UIListSelection(),
+                style="background-color:rgb(240,240,240)"
+            ),
+        ),
+        UIVisSelection(),
+        conditionalPanel(
+            "false", # always hide the download button
+            downloadButton("downloadData")
+        )
+    )
 }
