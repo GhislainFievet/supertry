87ad66ace1800e19fe1614e697505fdec37b264d
GhislainFievet <ghislain.fievet@gmail.com>
2022-10-21 06:33:27 +0200

    with library

--- R/supertry.r
+++ R/supertry.r
@@ -1,3 +1,13 @@
+library(shiny)
+library(ggplot2)
+library(Cairo)
+library(Seurat)
+library(shinycssloaders)
+library(plotly)
+library(shinyjs)
+library(DT)
+library(tidyverse)
+
 func_supertry = function(){
     message("hello")
 }
